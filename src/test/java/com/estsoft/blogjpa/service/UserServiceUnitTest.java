b256979523b5ee617bb2ede7545e58062edeff6f
songjaehoon <sjh101111@nate.com>
2024-04-18 21:10:57 +0900

    feat: sonarqube 연동 및 UnitTest코드 작성

--- src/test/java/com/estsoft/blogjpa/service/UserServiceUnitTest.java
+++ src/test/java/com/estsoft/blogjpa/service/UserServiceUnitTest.java
@@ -6,18 +6,16 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import com.estsoft.blogjpa.domain.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-class UserServiceTest2 {
+class UserServiceUnitTest {
     @Mock
     UserRepository userRepository;
 
