3b63340a09c3bdd84a224000f48d748b6197abaf
songjaehoon <sjh101111@nate.com>
2024-04-22 13:11:43 +0900

    feat: 배포 자동화를 위한 스크립트 파일 추가
    1. 깃허브 .yml 파일에 코드 추가
    2. appspec.yml, deploy.sh 파일 추가

--- src/test/java/com/estsoft/blogjpa/controller/BlogControllerUnitTest.java
+++ src/test/java/com/estsoft/blogjpa/controller/BlogControllerUnitTest.java
@@ -1,16 +1,26 @@
 package com.estsoft.blogjpa.controller;
 
+import com.estsoft.blogjpa.domain.Article;
 import com.estsoft.blogjpa.dto.AddArticleRequest;
 import com.estsoft.blogjpa.service.BlogService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(MockitoExtension.class)
 class BlogControllerUnitTest {
@@ -29,7 +39,22 @@ class BlogControllerUnitTest {
 
     @DisplayName("블로그 Article 생성")
     @Test
-    void testArticle() {
+    void testArticle() throws Exception {
         AddArticleRequest request = new AddArticleRequest("title", "content");
+        ObjectMapper objectMapper = new ObjectMapper();
+        String requestJson = objectMapper.writeValueAsString(request);
+
+        Mockito.doReturn(new Article("title", "content"))
+                .when(blogService).save(any(AddArticleRequest.class));
+
+        //when
+        ResultActions resultActions = mockMvc.perform(post("/api/articles")
+                        .content(requestJson).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated());
+
+        resultActions.andExpect(jsonPath("title").value(request.getTitle()));
+        resultActions.andExpect(jsonPath("content").value(request.getContent()));
     }
+
+//    @DisplayName() 실패할경우 테스트 코드
 }
